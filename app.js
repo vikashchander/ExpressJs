e2131207be3921438f32fdfe63405903fe04e76b
Vikash Chander <vikashchander78384@gmail.com>
2019-02-18 19:24:25 +0530

    add routers and html pages

--- app.js
+++ app.js
@@ -6,43 +6,23 @@ const myApp = express();
   }, function*/
 console.log("what is myApp ",myApp,typeof myApp);  
 
-const bodyParser = require("body-parser");  //  ???
+const bodyParser = require("body-parser");  // it is used to parse the body similar as jason.stringfy
 
-console.log("what is Express.static ",express.static("public"));  // ???
-app.use(express.static("public"));           //  ???
-app.use(bodyParser.urlencoded({extended:false})); //  ???
-
-app.get('/login',(req,res)=>{
-    console.log("req & res",req.query,res);
-    var userid =req.query.userid;
-    var pwd =req.query.pwd;
-    if(userid ==pwd){
-        res.send('welcome '+userid);
-    }else{
-        response.send('Invalid UserId or Password');
-    }
-})
-app.post('/login',(req,res)=>{
-    console.log("body is ",req.body);
-    var userid= req.body.userid;
-    var pwd = req.body.pwd;
-    if(userid ==pwd){
-        res.send("Welcome "+userid);
-    }else{
-        response.send('invalid Userid or Password');
-    }
-})
+console.log("what is Express.static ",express.static("public"));  // take a root directory
+app.use(express.static("public"));           //  app.use as middleware put inside the middleware 
+app.use(bodyParser.urlencoded({extended:false})); // extended false when single value or array and extended true when pass anything
+app.use('/',require("./routes/user"));
+app.use('/products',require());
 
 app.use((req,res,next)=>{       //what about next
-       response.send("U type something Wrong ...");
+       res.send("U type something Wrong ...");
 })
 
-myApp.listen(process.env.Port ||2345,(err)=>{
+myApp.listen(process.env.PORT ||2345,(err)=>{
     if(err){
         throw err;
     }
     else{
         console.log("server start");
     }
-    
 })
